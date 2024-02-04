2ff0729ddefa316e44d53e18de05f4f0ba40c556
Akshay Singh <singhakshay8081@gmail.com>
2024-02-04 23:35:37 +0530

    Add files via upload

---
+++ havin clause.sql
@@ -0,0 +1,22 @@
+create database Friends;
+use Friends;
+create table informFriend (
+	id int primary key,
+	name varchar(50),
+	age int,
+    city varchar(50)
+);
+insert into informFriend (id,name,age,city)
+values
+(101,"AKSHAY",20,"kanpur"),
+(102,"SHIVANI",21,"mirjapur"),
+(103,"AYUSHI",20,"noida"),
+(104,"BUTKI",23,"mirjapur");
+
+select * from informFriend limit 2;
+select avg(age) from informFriend;
+select city from informFriend group by city;
+select city,name, count(age) from informFriend group by city,name;
+select city,count(age) from informFriend group by city having max(age)>19;
+
+    
